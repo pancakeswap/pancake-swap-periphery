1a55993bbd6f1d89f004bdc235a5e54e43e647ca
eve-contract-person <tom.reclik@gmail.com>
2022-02-07 13:06:46 +0100

    Refactored ETH to FTM

--- contracts/interfaces/IWFTM.sol
+++ contracts/interfaces/IWFTM.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-interface IWETH {
+interface IWFTM {
     function deposit() external payable;
     function transfer(address to, uint value) external returns (bool);
     function withdraw(uint) external;
