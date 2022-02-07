6f9716863797c6f1cab5f879f7a9637ddb1002fd
eve-contract-person <tom.reclik@gmail.com>
2022-02-07 13:07:31 +0100

    Refactored ETH to FTM

--- contracts/test/WFTM9.sol
+++ contracts/test/WFTM9.sol
@@ -15,9 +15,9 @@
 
 pragma solidity =0.6.6;
 
-contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+contract WFTM9 {
+    string public name     = "Wrapped Fantom";
+    string public symbol   = "WFTM";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
